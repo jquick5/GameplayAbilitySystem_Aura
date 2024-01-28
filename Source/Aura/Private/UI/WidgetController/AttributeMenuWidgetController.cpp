d552352dcd2aca83d8b51560c0d3d5641b01d68f
Jay Quick <jay.quick79@gmail.com>
2024-01-28 12:53:14 -0800

    Asset manager, native gameplay tags and ability system library

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright JQX Software
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
